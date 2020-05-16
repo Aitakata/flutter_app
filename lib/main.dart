131b6a74ce4c0ab6f8273796a6d6eab9c57b739e
Artur Issam Takata <Artur.takata@gmail.com>
2020-05-16 03:44:28 -0300

    tratamento login V1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const req= "https://api-seetec.heroku.com.com/listarcursos";
 
 void main() {
+  // retornoApi();
   runApp(MaterialApp(
     home: Home(),
   ));
@@ -12,6 +18,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController login = TextEditingController();
+  TextEditingController senha = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +37,8 @@ class _HomeState extends State<Home> {
               size: 120.0,
               color: Colors.blue,
             ),
-            TextField(keyboardType: TextInputType.text,
+            TextField(
+              keyboardType: TextInputType.text,
               decoration: InputDecoration(
                   labelText: "Usuário:",
                   labelStyle: TextStyle(
@@ -37,8 +46,10 @@ class _HomeState extends State<Home> {
                     fontSize: 24.0,
                   )
               ) ,
+              controller: login,
             ),
-            TextField(keyboardType: TextInputType.text,
+            TextField(
+              keyboardType: TextInputType.number,
               decoration: InputDecoration(
                   labelText: "Senha:",
                   labelStyle: TextStyle(
@@ -46,21 +57,83 @@ class _HomeState extends State<Home> {
                     fontSize: 24.0,
                   )
               ),
+              obscureText: true,
+              controller: senha,
             ),
             Padding(
               padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
               child: Container(
                 child:  RaisedButton(
-                  onPressed: (){},
-                  child: Text("Logar"),
+                  onPressed: () {
+                    if (login.text == "admin" && senha.text == "1234"){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => bemvindo()));
+                    } else {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => erroLogin()));
+                    }
+                  },
+                  //onPressed: (){imprimir(login.text,senha.text);},
+                  child: Text("Logar",
+                     style: TextStyle(color:Colors.white, fontSize: 25.0)),
+                  color: Colors.blue,
                 ),
               ),
+            ),
+            Text(
+              "Esqueci a senha",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blue, fontSize: 25.0),
             ),
           ],
         ),
       )
     );
+    }
+    void imprimir (String usuario, String senha){
+    print("Usuario $usuario Senha $senha");
+  }
+}
+
+class bemvindo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    retornoApi();
+    return Scaffold (
+      appBar: AppBar(
+       title: Text("Seja bem vindo!"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          child: Text("Voltar"),
+        ),
+      ),
+    );
   }
 }
 
 
+class erroLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: Text("Usuário Inválido!"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          child: Text("Voltar"),
+        ),
+      ),
+    );
+  }
+}
+
+void retornoApi() async {
+ http.Response response = await http.get(req);
+ print (json.decode(response.body));
+}
